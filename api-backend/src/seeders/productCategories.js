47e4078f18948ad19c54e3dde0ead0d11ecf9d2a
christianmurillo <christian.murillo@neworientalclub88.com>
2020-02-06 03:48:18 +0800

    Added productCategories seeders

---
+++ api-backend/src/seeders/productCategories.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('productCategories', [
+      {
+        name: 'Accessories',
+        description: 'Accessories',
+        created_at: new Date(),
+        updated_at: new Date(),
+        is_deleted: 0
+      },
+      {
+        name: 'Helmets',
+        description: 'Helmets',
+        created_at: new Date(),
+        updated_at: new Date(),
+        is_deleted: 0
+      },
+      {
+        name: 'Parts',
+        description: 'Parts',
+        created_at: new Date(),
+        updated_at: new Date(),
+        is_deleted: 0
+      },
+      {
+        name: 'Riding Gear',
+        description: 'Riding Gear',
+        created_at: new Date(),
+        updated_at: new Date(),
+        is_deleted: 0
+      },
+      {
+        name: 'Tires',
+        description: 'Tires',
+        created_at: new Date(),
+        updated_at: new Date(),
+        is_deleted: 0
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('productCategories', null, {});
+  }
+};
