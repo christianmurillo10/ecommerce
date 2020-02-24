7fcd6c1bb5f199c690ca522f689ca541a19c83aa
christianmurillo <christian.murillo@neworientalclub88.com>
2020-02-25 06:03:08 +0800

    Added cart functionality in toolbar

--- website-frontend/src/components/core/Toolbar.vue
+++ website-frontend/src/components/core/Toolbar.vue
@@ -58,8 +58,8 @@
             <v-flex xs4 sm4 md4 lg4>
               <v-layout justify-end>
                 <v-tooltip right>
-                  <template v-slot:activator="{ on }">
-                    <v-btn icon v-on="on">
+                  <template v-slot:activator="onTooltip">
+                    <v-btn icon v-on="onTooltip.on">
                       <v-badge color="red" content="0">
                         <v-icon>mdi-heart</v-icon>
                       </v-badge>
@@ -67,16 +67,98 @@
                   </template>
                   <span>Wishlist</span>
                 </v-tooltip>
-                <v-tooltip right>
-                  <template v-slot:activator="{ on }">
-                    <v-btn icon v-on="on">
-                      <v-badge color="green" content="0">
-                        <v-icon>mdi-cart</v-icon>
-                      </v-badge>
-                    </v-btn>
+                <v-menu
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :nudge-width="200"
+                  offset-y
+                  transition="slide-x-reverse-transition"
+                >
+                  <template v-slot:activator="{ on: { click } }">
+                    <v-tooltip right>
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on:click="click" v-on="on">
+                          <v-badge color="green" :content="cartList.length">
+                            <v-icon>mdi-cart</v-icon>
+                          </v-badge>
+                        </v-btn>
+                      </template>
+                      <span>Cart</span>
+                    </v-tooltip>
                   </template>
-                  <span>Cart</span>
-                </v-tooltip>
+
+                  <v-card width="350" v-if="cartList.length !== 0">
+                    <v-layout row wrap justify-center>
+                      <v-card-title class="title">Cart Items</v-card-title>
+                    </v-layout>
+
+                    <v-divider></v-divider>
+
+                    <v-card-text class="pa-0">
+                      <v-simple-table fixed-header height="200px">
+                        <template v-slot:default>
+                          <tbody>
+                            <tr v-for="item in cartList" :key="item.name">
+                              <td>
+                                <v-img :src="item.file_path" max-height="60px" max-width="60px"></v-img>
+                              </td>
+                              <td>
+                                <v-row class="font-weight-medium">{{ item.name }}</v-row>
+                                <v-row>{{ `${item.quantity} x &#8369${item.price}` }}</v-row>
+                              </td>
+                              <td class="justify-center">
+                                <v-icon small @click="deleteItem(props.item.id)">mdi-delete</v-icon>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </template>
+                      </v-simple-table>
+                    </v-card-text>
+
+                    <v-divider></v-divider>
+
+                    <v-card-subtitle class="mx-10 mb-n5">
+                      <v-layout row wrap>
+                        <v-layout>
+                          <p class="font-weight-medium">Subtotal</p>
+                        </v-layout>
+                        <v-layout justify-end>
+                          <p class="font-weight-medium">&#8369 50000</p>
+                        </v-layout>
+                      </v-layout>
+                    </v-card-subtitle>
+
+                    <v-divider></v-divider>
+
+                    <v-layout row wrap justify-center>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          small
+                          color="blue-grey"
+                          outlined
+                          class="ma-2 white--text"
+                          @click="menu = false"
+                        >
+                          <v-icon left dark>mdi-cart</v-icon>VIEW CART
+                        </v-btn>
+                        <v-btn
+                          small
+                          color="blue-grey"
+                          class="ma-2 white--text"
+                          @click="menu = false"
+                        >
+                          <v-icon left dark>mdi-basket</v-icon>CHECKOUT
+                        </v-btn>
+                      </v-card-actions>
+                    </v-layout>
+                  </v-card>
+                  <v-card width="400" v-else>
+                    <v-layout row wrap justify-center>
+                      <v-card-title class="title">Your Cart is empty!</v-card-title>
+                    </v-layout>
+                  </v-card>
+                </v-menu>
               </v-layout>
             </v-flex>
           </v-layout>
@@ -91,7 +173,73 @@ import { mapState, mapActions, mapMutations } from "vuex";
 
 export default {
   data: () => ({
-    keywords: ""
+    keywords: "",
+    fav: true,
+    menu: false,
+    message: false,
+    hints: true,
+    cartList: [
+      {
+        name: "Frozen Yogurt Frozen Yogurt Frozen Yogurt",
+        quantity: 159,
+        price: 200,
+        file_path: require("../../assets/images/no-image.png")
+      },
+      {
+        name: "Ice cream sandwich",
+        quantity: 237,
+        price: 20,
+        file_path: require("../../assets/images/no-image.png")
+      },
+      {
+        name: "Eclair",
+        quantity: 262,
+        price: 100,
+        file_path: require("../../assets/images/no-image.png")
+      },
+      {
+        name: "Cupcake",
+        quantity: 305,
+        price: 25,
+        file_path: require("../../assets/images/no-image.png")
+      },
+      {
+        name: "Gingerbread",
+        quantity: 356,
+        price: 98,
+        file_path: require("../../assets/images/no-image.png")
+      },
+      {
+        name: "Jelly bean",
+        quantity: 375,
+        price: 156,
+        file_path: require("../../assets/images/no-image.png")
+      },
+      {
+        name: "Lollipop",
+        quantity: 392,
+        price: 10,
+        file_path: require("../../assets/images/no-image.png")
+      },
+      {
+        name: "Honeycomb",
+        quantity: 408,
+        price: 36,
+        file_path: require("../../assets/images/no-image.png")
+      },
+      {
+        name: "Donut",
+        quantity: 452,
+        price: 88,
+        file_path: require("../../assets/images/no-image.png")
+      },
+      {
+        name: "KitKat",
+        quantity: 518,
+        price: 250,
+        file_path: require("../../assets/images/no-image.png")
+      }
+    ]
   }),
   computed: {
     ...mapState("appbar", ["primaryDrawer"]),
