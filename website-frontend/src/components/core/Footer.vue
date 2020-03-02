d628c951695d03b98f365858cb24e2bf695eca89
christianmurillo <christian.murillo@neworientalclub88.com>
2020-03-02 23:01:47 +0800

    Updatef footer

--- website-frontend/src/components/core/Footer.vue
+++ website-frontend/src/components/core/Footer.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- <v-footer color="orange lighten-1" dark> -->
   <v-footer dark padless>
     <v-container class="col-lg-10 offset-lg-1">
       <v-layout row wrap>
@@ -9,11 +8,7 @@
               <v-flex xs2 sm2 md2 lg2>
                 <v-container>
                   <router-link to="/">
-                    <v-img
-                      :src="avatar"
-                      max-height="50px"
-                      max-width="50px"
-                    ></v-img>
+                    <v-img :src="avatar" max-height="50px" max-width="50px"></v-img>
                   </router-link>
                 </v-container>
               </v-flex>
@@ -23,7 +18,9 @@
                 </v-container>
               </v-flex>
               <v-flex xs12 sm12 md12 lg12>
-                <p class="caption">{{ companyDescription }}</p>
+                <v-container>
+                  <p class="caption">{{ companyDescription }}</p>
+                </v-container>
               </v-flex>
             </v-layout>
           </v-container>
@@ -33,61 +30,78 @@
           <v-container>
             <v-layout row wrap>
               <v-flex xs12 sm12 md12 lg12>
-                <h4>CONTACT INFO</h4>
-              </v-flex>
-              <v-flex xs12 sm12 md12 lg12>
-                <div>
-                  <span class="caption grey--text">Address: </span> <br />
-                  <p class="caption">{{ contactInfo.address }}</p>
-                </div>
-                <div>
-                  <span class="caption grey--text">Phone: </span> <br />
-                  <p class="caption">{{ contactInfo.phone }}</p>
-                </div>
-                <div>
-                  <span class="caption grey--text">Email: </span> <br />
-                  <a class="caption" :href="`mailto:${contactInfo.email}`">{{
-                    contactInfo.email
-                  }}</a>
-                </div>
+                <v-container>
+                  <h4>CONTACT INFO</h4>
+                  <div>
+                    <span class="caption grey--text">Address:</span>
+                    <br />
+                    <p class="caption">{{ contactInfo.address }}</p>
+                  </div>
+                  <div>
+                    <span class="caption grey--text">Phone:</span>
+                    <br />
+                    <p class="caption">{{ contactInfo.phone }}</p>
+                  </div>
+                  <div>
+                    <span class="caption grey--text">Email:</span>
+                    <br />
+                    <a class="caption" :href="`mailto:${contactInfo.email}`">
+                      {{
+                      contactInfo.email
+                      }}
+                    </a>
+                  </div>
+                </v-container>
               </v-flex>
             </v-layout>
           </v-container>
         </v-flex>
         <v-flex xs1 sm1 md1 lg1></v-flex>
-        <v-flex xs3 sm3 md3 lg3>
+        <v-flex xs12 sm12 md3 lg3>
           <v-container>
             <v-layout row wrap>
-              <h4>QUICK LINKS</h4>
+              <v-flex xs12 sm12 md12 lg12>
+                <v-container>
+                  <h4>QUICK LINKS</h4>
+                  <v-container>
+                    <v-row v-for="(link, i) in links" :key="i">
+                      <v-hover>
+                        <router-link
+                          slot-scope="{ hover }"
+                          :class="`${hover ? 'grey' : 'white'}--text`"
+                          style="text-decoration: none;"
+                          :to="link.url"
+                        >
+                          <span class="caption">{{ link.name }}</span>
+                        </router-link>
+                      </v-hover>
+                    </v-row>
+                  </v-container>
+                </v-container>
+              </v-flex>
             </v-layout>
           </v-container>
         </v-flex>
       </v-layout>
     </v-container>
     <v-card flat tile width="100%" class="black text-center">
-      <!-- <v-divider></v-divider> -->
+      <v-card-title class="grey darken-3">
+        <div class="col-lg-10 offset-lg-1">
+          <v-layout row>
+            <strong class="subheading">Get connected with us on social networks!</strong>
+            <v-spacer></v-spacer>
+            <v-btn v-for="(social, i) in socials" :key="i" class="mx-4" dark icon>
+              <v-icon size="24px">{{ social.icon }}</v-icon>
+            </v-btn>
+          </v-layout>
+        </div>
+      </v-card-title>
 
-      <v-card-text class="white--text">
-        © {{ new Date().getFullYear() }} — <strong> E-Commerce</strong>
+      <v-card-text class="py-2 white--text text-center">
+        © {{ new Date().getFullYear() }} —
+        <strong>E-Commerce</strong>
       </v-card-text>
     </v-card>
-    <!-- <v-row justify="center" no-gutters>
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        text
-        rounded
-        class="my-2"
-        >{{ link }}</v-btn
-      >
-      <v-col class="black lighten-1 py-4 text-center white--text" cols="12">
-        <p>
-          {{ new Date().getFullYear() }} —
-          <strong>ARK</strong>
-        </p>
-      </v-col>
-    </v-row> -->
   </v-footer>
 </template>
 
@@ -95,20 +109,27 @@
 export default {
   data: () => ({
     companyDescription:
-      "E-Commerce is an online membership shopping destination that offers direct access to a wide selection of quality products at discounted prices and at the same time offers the opportunity for its members an accessible and sustainable income via the e-commerce industry.",
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.",
     contactInfo: {
       address:
         "4th floor Visual Color Building, 101 Kalayaan Avenue Central Quezon City 2nd District, Philippines",
       phone: "09478850164 | 09451107821",
-      email: "talktous.ark@gmail.com"
+      email: "talktous.ecommerce@gmail.com"
     },
     links: [
-      "Home",
-      "Categories",
-      "Products",
-      "Terms and Conditions",
-      "About Us",
-      "Contact Us"
+      { name: "Home", url: "/" },
+      { name: "Categories", url: "/" },
+      { name: "Products", url: "/" },
+      { name: "Terms and Conditions", url: "/" },
+      { name: "About Us", url: "/" },
+      { name: "Contact Us", url: "/" }
+    ],
+    socials: [
+      { icon: "mdi-facebook", url: "/" },
+      { icon: "mdi-twitter", url: "/" },
+      { icon: "mdi-google-plus", url: "/" },
+      { icon: "mdi-linkedin", url: "/" },
+      { icon: "mdi-instagram", url: "/" }
     ]
   }),
   computed: {
