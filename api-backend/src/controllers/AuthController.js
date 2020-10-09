591f16f465a88c15be8505d25f68778b6375d800
christianmurillo10 <christianoroceomurillo@gmail.com>
2020-10-09 18:18:25 +0800

    [Refactor Backend] Implemented error handling in AuthController

--- api-backend/src/controllers/AuthController.js
+++ api-backend/src/controllers/AuthController.js
@@ -11,326 +11,329 @@ const {
 
 module.exports = {
   /**
-   * Login Account
-   * @param req
-   * @param res
+   * Login
    * @routes POST /users/login
-   * @returns {never}
    */
   login: async (req, res) => {
     let ip = req.headers["x-forwarded-for"] || req.ip;
     let params = req.body;
-    let criteria, data, token;
-
-    // Validators
-    if (_.isUndefined(params.username))
-      return res.badRequest("Invalid Credentials.");
+    let statusCode = 200,
+      errors = [],
+      criteria,
+      data,
+      token;
 
     try {
       // Validators
-      if (_.isEmpty(params.username))
-        return cb(null, { error: true, message: "Username is required." });
-      if (_.isEmpty(params.password))
-        return cb(null, { error: true, message: "Password is required." });
-
-      // Pre-setting variables
-      criteria = { where: { username: params.username } };
-      // Execute findAll query
-      user = await Model.Users.findAll(criteria);
-      // Account checker
-      if (!_.isEmpty(user[0])) {
-        let userInfo = user[0].get({ plain: true });
-        let passwordCompare = await bcrypt.comparePassword(
-          params.password,
-          userInfo.password
-        );
-        if (passwordCompare) {
-          // Update login status
-          let updatedUser = await user[0].update({ is_logged: YES });
-          data = updatedUser.get({ plain: true });
-          token = await jwt.generateToken({ id: data.id, type: "user" });
-          console.log(
-            "AuthController@login - [ID]:%s [User]:%s [IP]%s",
-            updatedUser.id,
-            updatedUser.username,
-            ip
-          );
-
-          res.json({
-            status: 200,
-            message: "User successfully signed in.",
-            result: {
-              token: token,
-              data: _.omit(data, [
-                "password",
-                "created_at",
-                "updated_at",
-                "permission_type",
-                "is_logged",
-                "is_active",
-                "is_deleted",
-              ]),
-            },
-          });
-        } else {
-          res.json({
-            status: 200,
-            message: "Invalid Password.",
-            result: false,
-          });
-        }
-      } else {
-        res.json({
-          status: 200,
-          message: "Invalid Username.",
-          result: false,
-        });
+      if (_.isEmpty(params)) {
+        statusCode = 400;
+        errors.push("Please check your input.");
+        throw new Error("Bad Request");
       }
+      if (_.isEmpty(params.username)) errors.push("Username is required.");
+      if (_.isEmpty(params.password)) errors.push("Password is required.");
+      if (errors.length > 0) {
+        statusCode = 400;
+        throw new Error("Bad Request");
+      }
+
+      // Validate Account
+      criteria = { where: { username: params.username } };
+      user = await Model.Users.findAll(criteria);
+      if (_.isEmpty(user[0])) {
+        statusCode = 400;
+        errors.push("Invalid Username.");
+        throw new Error("Bad Request");
+      }
+
+      // Validate Password
+      let userInfo = user[0].get({ plain: true });
+      let passwordCompare = await bcrypt.comparePassword(
+        params.password,
+        userInfo.password
+      );
+      if (!passwordCompare) {
+        statusCode = 400;
+        errors.push("Invalid Password.");
+        throw new Error("Bad Request");
+      }
+
+      // Update login status
+      let updatedUser = await user[0].update({ is_logged: YES });
+      data = updatedUser.get({ plain: true });
+      token = await jwt.generateToken({ id: data.id, type: "user" });
+      console.log(
+        "AuthController@login - [ID]:%s [User]:%s [IP]%s",
+        updatedUser.id,
+        updatedUser.username,
+        ip
+      );
+
+      res.status(statusCode).send({
+        message: "User successfully signed in.",
+        result: {
+          token: token,
+          data: _.omit(data, [
+            "password",
+            "created_at",
+            "updated_at",
+            "permission_type",
+            "is_logged",
+            "is_active",
+            "is_deleted",
+          ]),
+        },
+      });
     } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed to signin account.",
+      res.status(statusCode).send({
+        message: err.message.toString(),
+        errors: errors,
       });
     }
   },
 
   /**
-   * Logout Account
-   * @param req
-   * @param res
-   * @returns {Promise<void>}
+   * Logout
    * @routes POST /users/logout
    */
   logout: async (req, res) => {
     let ip = req.headers["x-forwarded-for"] || req.ip;
-    var token = req.body.token; // Value needs to be changed, so keep it to `var`
+    let params = req.body;
+    let statusCode = 200,
+      errors = [],
+      token = params.token;
 
     try {
-      if (token) {
-        let tokenData = await jwt.verifyToken(token);
-        if (!tokenData) {
-          res.json({
-            status: 200,
-            message: "Already logged out.",
-            result: false,
-          });
-        }
-
-        let user = await Model.Users.findByPk(tokenData.id);
-        // Update login status
-        let updatedUser = await user.update({ is_logged: NO });
-        console.log(
-          "AuthController@logout - [ID]:%s [User]:%s [IP]%s",
-          updatedUser.id,
-          updatedUser.username,
-          ip
-        );
-
-        res.json({
-          status: 200,
-          message: "Successfully signed out.",
-          result: true,
-        });
+      // Validators
+      if (_.isEmpty(params)) {
+        statusCode = 400;
+        errors.push("Please check your input.");
+        throw new Error("Bad Request");
       }
+
+      if (_.isEmpty(token)) {
+        statusCode = 400;
+        errors.push("Token is required.");
+        throw new Error("Bad Request");
+      }
+
+      let tokenData = await jwt.verifyToken(token);
+      if (_.isNull(tokenData)) {
+        statusCode = 400;
+        errors.push("Invalid Token.");
+        throw new Error("Bad Request");
+      }
+
+      let user = await Model.Users.findByPk(tokenData.id);
+      let updatedUser = await user.update({ is_logged: NO });
+      console.log(
+        "AuthController@logout - [ID]:%s [User]:%s [IP]%s",
+        updatedUser.id,
+        updatedUser.username,
+        ip
+      );
+
+      res.status(statusCode).send({
+        message: "Successfully signed out.",
+        result: [],
+      });
     } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed to signout account.",
+      res.status(statusCode).send({
+        message: err.message.toString(),
+        errors: errors,
       });
     }
   },
 
   /**
-   * Customer Login Account
-   * @param req
-   * @param res
+   * Customer Login
    * @routes POST /customers/login
-   * @returns {never}
    */
   customerLogin: async (req, res) => {
     let ip = req.headers["x-forwarded-for"] || req.ip;
     let params = req.body;
-    let criteria, data, token;
-
-    // Validators
-    if (_.isUndefined(params.email))
-      return res.badRequest("Invalid Credentials.");
+    let statusCode = 200,
+      errors = [],
+      criteria,
+      data,
+      token;
 
     try {
       // Validators
-      if (_.isEmpty(params.email))
-        return cb(null, { error: true, message: "Email is required." });
-      if (_.isEmpty(params.password))
-        return cb(null, { error: true, message: "Password is required." });
-
-      // Pre-setting variables
-      criteria = { where: { email: params.email, is_deleted: NO } };
-      // Execute findAll query
-      customer = await Model.Customers.findAll(criteria);
-      // Account checker
-      if (!_.isEmpty(customer[0])) {
-        let customerInfo = customer[0].get({ plain: true });
-        let passwordCompare = await bcrypt.comparePassword(
-          params.password,
-          customerInfo.password
-        );
-        if (passwordCompare) {
-          if (
-            customerInfo.status === CUSTOMER_STATUS_APPROVED &&
-            customerInfo.is_active === YES
-          ) {
-            // Update login status
-            let updatedCustomer = await customer[0].update({ is_logged: YES });
-            data = updatedCustomer.get({ plain: true });
-            token = await jwt.generateToken({ id: data.id, type: "customer" });
-            console.log(
-              "AuthController@customerLogin - [ID]:%s [Customer]:%s [IP]%s",
-              updatedCustomer.id,
-              updatedCustomer.email,
-              ip
-            );
-
-            res.json({
-              status: 200,
-              message: "Customer successfully signed in.",
-              result: {
-                token: token,
-                data: _.omit(data, [
-                  "password",
-                  "created_at",
-                  "updated_at",
-                  "status",
-                  "is_logged",
-                  "is_active",
-                  "is_deleted",
-                ]),
-              },
-            });
-          } else if (customerInfo.is_active === NO) {
-            res.json({
-              status: 200,
-              message:
-                "Your account is inactive, please contact administration to activate your account.",
-              result: false,
-            });
-          } else {
-            res.json({
-              status: 200,
-              message: "Your account is not yet activated.",
-              result: false,
-            });
-          }
-        } else {
-          res.json({
-            status: 200,
-            message: "Invalid Password.",
-            result: false,
-          });
-        }
-      } else {
-        res.json({
-          status: 200,
-          message: "Invalid Email.",
-          result: false,
-        });
+      if (_.isEmpty(params)) {
+        statusCode = 400;
+        errors.push("Please check your input.");
+        throw new Error("Bad Request");
       }
-    } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed to sign in account.",
-      });
-    }
-  },
+      if (_.isEmpty(params.email)) errors.push("Email is required.");
+      if (_.isEmpty(params.password)) errors.push("Password is required.");
+      if (errors.length > 0) {
+        statusCode = 400;
+        throw new Error("Bad Request");
+      }
 
-  /**
-   * Customer Logout Account
-   * @param req
-   * @param res
-   * @returns {Promise<void>}
-   * @routes POST /customers/logout
-   */
-  customerlogout: async (req, res) => {
-    let ip = req.headers["x-forwarded-for"] || req.ip;
-    var token = req.body.token; // Value needs to be changed, so keep it to `var`
+      // Validate Account
+      criteria = { where: { email: params.email, is_deleted: NO } };
+      customer = await Model.Customers.findAll(criteria);
+      if (_.isEmpty(customer[0])) {
+        statusCode = 400;
+        errors.push("Invalid Email.");
+        throw new Error("Bad Request");
+      }
 
-    try {
-      if (token) {
-        let tokenData = await jwt.verifyToken(token);
-        if (!tokenData) {
-          res.json({
-            status: 200,
-            message: "Already logged out.",
-            result: false,
-          });
-        }
+      // Validate Password
+      let customerInfo = customer[0].get({ plain: true });
+      let passwordCompare = await bcrypt.comparePassword(
+        params.password,
+        customerInfo.password
+      );
+      if (!passwordCompare) {
+        statusCode = 400;
+        errors.push("Invalid Password.");
+        throw new Error("Bad Request");
+      }
 
-        let customer = await Model.Customers.findByPk(tokenData.id);
+      if (
+        customerInfo.status === CUSTOMER_STATUS_APPROVED &&
+        customerInfo.is_active === YES
+      ) {
         // Update login status
-        let updatedCustomer = await customer.update({ is_logged: NO });
+        let updatedCustomer = await customer[0].update({ is_logged: YES });
+        data = updatedCustomer.get({ plain: true });
+        token = await jwt.generateToken({ id: data.id, type: "customer" });
         console.log(
-          "AuthController@customerlogout - [ID]:%s [Customer]:%s [IP]%s",
+          "AuthController@customerLogin - [ID]:%s [Customer]:%s [IP]%s",
           updatedCustomer.id,
           updatedCustomer.email,
           ip
         );
 
-        res.json({
-          status: 200,
-          message: "Successfully signed out.",
-          result: true,
+        res.status(statusCode).send({
+          message: "Customer successfully signed in.",
+          result: {
+            token: token,
+            data: _.omit(data, [
+              "password",
+              "created_at",
+              "updated_at",
+              "status",
+              "is_logged",
+              "is_active",
+              "is_deleted",
+            ]),
+          },
         });
+      } else if (customerInfo.is_active === NO) {
+        statusCode = 500;
+        errors.push(
+          "Your account is inactive, please contact administration to activate your account."
+        );
+        throw new Error("Something Went Wrong");
+      } else {
+        statusCode = 500;
+        errors.push("Your account is not yet activated.");
+        throw new Error("Something Went Wrong");
       }
     } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed to sign out account.",
+      res.status(statusCode).send({
+        message: err.message.toString(),
+        errors: errors,
+      });
+    }
+  },
+
+  /**
+   * Customer Logout
+   * @routes POST /customers/logout
+   */
+  customerlogout: async (req, res) => {
+    let ip = req.headers["x-forwarded-for"] || req.ip;
+    let params = req.body;
+    let statusCode = 200,
+      errors = [],
+      token = params.token;
+
+    try {
+      // Validators
+      if (_.isEmpty(params)) {
+        statusCode = 400;
+        errors.push("Please check your input.");
+        throw new Error("Bad Request");
+      }
+
+      if (_.isEmpty(token)) {
+        statusCode = 400;
+        errors.push("Token is required.");
+        throw new Error("Bad Request");
+      }
+
+      let tokenData = await jwt.verifyToken(token);
+      if (_.isNull(tokenData)) {
+        statusCode = 400;
+        errors.push("Invalid Token.");
+        throw new Error("Bad Request");
+      }
+
+      let customer = await Model.Customers.findByPk(tokenData.id);
+      let updatedCustomer = await customer.update({ is_logged: NO });
+      console.log(
+        "AuthController@customerlogout - [ID]:%s [Customer]:%s [IP]%s",
+        updatedCustomer.id,
+        updatedCustomer.email,
+        ip
+      );
+
+      res.status(statusCode).send({
+        message: "Successfully signed out.",
+        result: [],
+      });
+    } catch (err) {
+      res.status(statusCode).send({
+        message: err.message.toString(),
+        errors: errors,
       });
     }
   },
 
   /**
    * Validate Token
-   * @param req
-   * @param res
-   * @returns {Promise<void>}
    * @routes POST /authorizations/validateToken
    */
   validateToken: async (req, res) => {
-    // let ip = req.headers["x-forwarded-for"] || req.ip;
-    var token = req.body.token; // Value needs to be changed, so keep it to `var`
+    let params = req.body;
+    let statusCode = 200,
+      errors = [],
+      token = params.token;
 
     try {
-      if (token) {
-        let tokenData = await jwt.verifyToken(token);
-        if (tokenData) {
-          res.json({
-            status: 200,
-            message: "Token Exist.",
-            result: true,
-          });
-        } else {
-          res.json({
-            status: 200,
-            message: "Token Already Expired.",
-            result: false,
-          });
-        }
-      } else {
-        res.json({
-          status: 200,
-          message: "Token Not Exist.",
-          result: false,
-        });
+      // Validators
+      if (_.isEmpty(params)) {
+        statusCode = 400;
+        errors.push("Please check your input.");
+        throw new Error("Bad Request");
       }
+
+      if (_.isEmpty(token)) {
+        statusCode = 400;
+        errors.push("Token is required.");
+        throw new Error("Bad Request");
+      }
+
+      let tokenData = await jwt.verifyToken(token);
+      if (_.isNull(tokenData)) {
+        statusCode = 400;
+        errors.push("Invalid Token.");
+        throw new Error("Bad Request");
+      }
+      
+      res.status(statusCode).send({
+        message: "Token Exist.",
+        result: [],
+      });
     } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed to signout account.",
+      res.status(statusCode).send({
+        message: err.message.toString(),
+        errors: errors,
       });
     }
   },
@@ -339,7 +342,10 @@ module.exports = {
     if (req.user) {
       next();
     } else {
-      return res.status(401).json({ message: "Unauthorized user!" });
+      return res.status(401).json({
+        message: "Unauthorized",
+        errors: [],
+      });
     }
   },
 };
