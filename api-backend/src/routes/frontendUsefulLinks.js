75f8cfaa5a8d6323731cfaee5bb83c83be2341e2
christianmurillo10 <christianoroceomurillo@gmail.com>
2020-10-15 12:24:21 +0800

    [Refactor Backend] Implemented error handling in FrontendUsefulLinksController on API

--- api-backend/src/routes/frontendUsefulLinks.js
+++ api-backend/src/routes/frontendUsefulLinks.js
@@ -10,7 +10,6 @@ router.route('/').get(frontendUsefulLinksController.findAll);
 router.route('/create').post(authController.authorization, frontendUsefulLinksController.create);
 router.route('/update/:id').put(authController.authorization, frontendUsefulLinksController.update);
 router.route('/delete/:id').put(authController.authorization, frontendUsefulLinksController.delete);
-router.route('/search/:value').get(authController.authorization, frontendUsefulLinksController.search);
 router.route('/:id').get(authController.authorization, frontendUsefulLinksController.findById);
 
 module.exports = router;
