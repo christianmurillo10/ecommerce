470416265f6908bc987c58bd4a234424ea15688d
christianmurillo10 <christianoroceomurillo@gmail.com>
2020-10-21 15:41:11 +0800

    [Refactor Backend] Implemented error handling in UsersController on API

--- api-backend/src/controllers/UsersController.js
+++ api-backend/src/controllers/UsersController.js
@@ -1,265 +1,283 @@
-const Model = require('../models');
-const { NO, YES } = require('../helpers/constant-helper');
+const Model = require("../models");
+const { ErrorHandler, handleSuccess } = require("../helpers/response-helper");
+const bcrypt = require("../helpers/bcrypt-helper");
+const { NO, YES } = require("../helpers/constant-helper");
 
 module.exports = {
   /**
-   * Create user
-   * @param req
-   * @param res
-   * @returns {Promise<void>}
+   * Create
    * @routes POST /users/create
    */
-  create: async (req, res) => {
+  create: async (req, res, next) => {
     const params = req.body;
-    const usernameChecker = /^[-\w ]+$/;
+    const usernameChecker = /^(?=[a-zA-Z0-9._]{6,30}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
     const emailChecker = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let criteria, initialValues, data;
-
-    // Validators
-    if (_.isUndefined(params))
-      return res.badRequest({ err: "Invalid Parameter: [params]" });
-    if (_.isEmpty(params))
-      return res.badRequest({ err: "Empty Parameter: [params]" });
-
-    // Override variables
-    params.created_at = moment().utc(8).format('YYYY-MM-DD HH:mm:ss');
-    params.role_id = _.isUndefined(params.role_id) ? 1 : params.role_id;
+    let errors = [],
+      criteria,
+      initialValues,
+      data;
 
     try {
       // Validators
-      if (_.isEmpty(params.username)) return res.json({ status: 200, message: "Username is required.", result: false });
-      if (_.isEmpty(params.password)) return res.json({ status: 200, message: "Password is required.", result: false });
-      if (_.isEmpty(params.email)) return res.json({ status: 200, message: "Email is required.", result: false });
-      if (params.username.length > 30) return res.json({ status: 200, message: "Username exceed 30 characters.", result: false });
-      if (params.username.length < 6) return res.json({ status: 200, message: "The username must be at least 6 characters.", result: false });
-      if (!usernameChecker.test(params.username)) return res.json({ status: 200, message: "Invalid username format.", result: false });
-      if (!emailChecker.test(params.email)) return res.json({ status: 200, message: "Invalid email format.", result: false });
-
-      // Pre-setting variables
-      criteria = { where: { $or: [{ email: params.email }, { username: params.username }] } };
-      initialValues = _.pick(params, ['email', 'username', 'password', 'role_id', 'created_at']);
-      // Execute findAll query
-      data = await Model.Users.findAll(criteria);
-      if (_.isEmpty(data[0])) {
-        let finalData = await Model.Users.create(initialValues);
-        res.json({
-          status: 200,
-          message: "Successfully created an account.",
-          result: _.omit(finalData.get({ plain: true }), ['password', 'is_deleted'])
-        });
-      } else {
-        res.json({
-          status: 200,
-          message: "User already exist.",
-          result: false
-        });
+      if (_.isEmpty(params)) {
+        errors.push("Invalid Parameter.");
+        throw new ErrorHandler(400, errors);
       }
-    } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed creating an account."
-      });
-    }
-  },
 
-  /**
-   * Update user
-   * @route PUT /users/update/:id
-   * @param req
-   * @param res
-   * @returns {never}
-   */
-  update: async (req, res) => {
-    const params = req.body;
-    let initialValues, data;
+      // Override variables
+      params.created_at = moment().utc(8).format("YYYY-MM-DD HH:mm:ss");
+      params.role_id = _.isUndefined(params.role_id) ? 1 : params.role_id;
 
-    if (_.isUndefined(params))
-      return res.badRequest({ err: "Invalid Parameter: [params]" });
-    if (_.isEmpty(params))
-      return res.badRequest({ err: "Empty Parameter: [params]" });
-
-    try {
-      // Pre-setting variables
-      initialValues = _.pick(params, ['email', 'username', 'password', 'role_id']);
-      // Execute findByPk query
-      data = await Model.Users.findByPk(req.params.id);
-      if (!_.isEmpty(data)) {
-        let finalData = await data.update(initialValues);
-        res.json({
-          status: 200,
-          message: "Successfully updated an account.",
-          result: finalData
-        });
-      } else {
-        res.json({
-          status: 200,
-          message: "User doesn't exist.",
-          result: false
-        });
+      if (_.isEmpty(params.username)) errors.push("Username is required.");
+      if (_.isEmpty(params.password)) errors.push("Password is required.");
+      if (_.isEmpty(params.email)) errors.push("Email is required.");
+      if (params.username && params.username.length > 30)
+        errors.push("Username exceed 30 characters.");
+      if (params.username && params.username.length < 6)
+        errors.push("Username must be at least 6 characters.");
+      if (!usernameChecker.test(params.username))
+        errors.push("Invalid username format.");
+      if (!emailChecker.test(params.email))
+        errors.push("Invalid email format.");
+      if (errors.length > 0) {
+        throw new ErrorHandler(400, errors);
       }
-    } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed updating an account."
-      });
-    }
-  },
 
-  /**
-   * Delete user
-   * @route PUT /users/delete/:id
-   * @param req
-   * @param res
-   * @returns {never}
-   */
-  delete: async (req, res) => {
-    let data;
-
-    try {
-      // Execute findByPk query
-      data = await Model.Users.findByPk(req.params.id);
-      if (!_.isEmpty(data)) {
-        let finalData = await data.update({ is_deleted: YES });
-        res.json({
-          status: 200,
-          message: "Successfully deleted an account.",
-          result: finalData
-        });
-      } else {
-        res.json({
-          status: 200,
-          message: "User doesn't exist.",
-          result: false
-        });
-      }
-    } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed deleting an account."
-      });
-    }
-  },
-
-  /**
-   * Search users
-   * @route POST /users/search/:value
-   * @param req
-   * @param res
-   * @returns {never}
-   */
-  search: async (req, res) => {
-    const params = req.params;
-    let query, data;
-
-    if (_.isUndefined(params))
-      return res.badRequest({ err: "Invalid Parameter: [params]" });
-    if (_.isEmpty(params))
-      return res.badRequest({ err: "Empty Parameter: [params]" });
-
-    try {
-      // Pre-setting variables
-      query = `SELECT id, email, username, role_id, created_at, updated_at, permission_type, is_logged, is_active FROM users WHERE CONCAT(email, username) LIKE ? AND is_deleted = ${NO};`;
-      // Execute native query
-      data = await Model.sequelize.query(query, {
-        replacements: [`%${params.value}%`],
-        type: Model.sequelize.QueryTypes.SELECT
-      });
-      if (!_.isEmpty(data)) {
-        res.json({
-          status: 200,
-          message: "Successfully searched data.",
-          result: data
-        });
-      } else {
-        res.json({
-          status: 200,
-          message: "No Data Found.",
-          result: false
-        });
-      }
-    } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed to search data."
-      });
-    }
-  },
-
-  /**
-   * Find all users
-   * @route GET /users
-   * @param req
-   * @param res
-   * @returns {never}
-   */
-  findAll: async (req, res) => {
-    let data, criteria;
-
-    try {
-      // Pre-setting variables
-      criteria = { where: { is_deleted: NO }, include: [{ model: Model.Roles, as: 'roles' }] };
-      // Execute findAll query
+      // Validate Data
+      criteria = {
+        where: {
+          $or: [{ email: params.email }, { username: params.username }],
+        },
+      };
       data = await Model.Users.findAll(criteria);
       if (!_.isEmpty(data[0])) {
-        res.json({
-          status: 200,
-          message: "Successfully find all data.",
-          result: data
-        });
-      } else {
-        res.json({
-          status: 200,
-          message: "No Data Found.",
-          result: false
-        });
+        errors.push("Data already exist.");
+        throw new ErrorHandler(500, errors);
       }
-    } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed to find all data."
+
+      // Pre-setting variables
+      initialValues = _.pick(params, [
+        "email",
+        "username",
+        "password",
+        "role_id",
+        "created_at",
+      ]);
+      let finalData = await Model.Users.create(initialValues);
+
+      handleSuccess(res, {
+        statusCode: 201,
+        message: "Successfully created data.",
+        result: _.omit(finalData.get({ plain: true }), [
+          "password",
+          "is_deleted",
+        ]),
       });
+    } catch (err) {
+      next(err);
     }
   },
 
   /**
-   * Find user by id
-   * @route GET /users/:id
-   * @param req
-   * @param res
-   * @returns {never}
+   * Update
+   * @route PUT /users/update/:id
    */
-  findById: async (req, res) => {
-    let data, criteria;
+  update: async (req, res, next) => {
+    const params = req.body;
+    let errors = [],
+      initialValues,
+      data;
 
     try {
-      // Pre-setting variables
-      criteria = { include: [{ model: Model.Roles, as: 'roles' }] };
-      // Execute findAll query
-      data = await Model.Users.findByPk(req.params.id, criteria);
-      if (!_.isEmpty(data)) {
-        res.json({
-          status: 200,
-          message: "Successfully find data.",
-          result: _.omit(data.get({ plain: true }), ['password', 'is_deleted'])
-        });
-      } else {
-        res.json({
-          status: 200,
-          message: "No Data Found.",
-          result: false
-        });
+      // Validators
+      if (_.isEmpty(params)) {
+        errors.push("Invalid Parameter.");
+        throw new ErrorHandler(400, errors);
       }
-    } catch (err) {
-      res.json({
-        status: 401,
-        err: err,
-        message: "Failed to find data."
+
+      // Override Variables
+      params.updated_at = moment().utc(8).format("YYYY-MM-DD HH:mm:ss");
+
+      // Validate Data
+      data = await Model.Users.findByPk(req.params.id);
+      if (_.isEmpty(data)) {
+        errors.push("Data doesn't exist.");
+        throw new ErrorHandler(500, errors);
+        y;
+      }
+
+      // Pre-setting variables
+      initialValues = _.pick(params, [
+        "email",
+        "username",
+        "role_id",
+        "updated_at",
+      ]);
+      let finalData = await data.update(initialValues);
+
+      handleSuccess(res, {
+        statusCode: 200,
+        message: "Successfully updated data.",
+        result: finalData,
       });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  /**
+   * Delete
+   * @route PUT /users/delete/:id
+   */
+  delete: async (req, res, next) => {
+    let errors = [],data;
+
+    try {
+      // Validate Data
+      data = await Model.Users.findByPk(req.params.id);
+      if (_.isEmpty(data)) {
+        errors.push("Data doesn't exist.");
+        throw new ErrorHandler(500, errors);
+      }
+      let finalData = await data.update({ is_deleted: YES });
+
+      handleSuccess(res, {
+        statusCode: 200,
+        message: "Successfully deleted an account.",
+        result: finalData,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  /**
+   * Change Password
+   * @route PUT /users/changePassword/:id
+   */
+  changePassword: async (req, res, next) => {
+    const params = req.body;
+    let errors = [],
+      initialValues,
+      data,
+      compareOldPassword,
+      compareNewPassword;
+
+    try {
+      // Validators
+      if (_.isEmpty(params)) {
+        errors.push("Invalid Parameter.");
+        throw new ErrorHandler(400, errors);
+      }
+      if (_.isEmpty(params.old_password))
+        errors.push("Old Password is required.");
+      if (_.isEmpty(params.new_password))
+        errors.push("New Password is required.");
+      if (errors.length > 0) {
+        throw new ErrorHandler(400, errors);
+      }
+
+      // Override variables
+      params.updated_at = moment().utc(8).format("YYYY-MM-DD HH:mm:ss");
+      params.password = params.new_password;
+
+      // Validate Data
+      data = await Model.Users.findByPk(req.params.id);
+      if (_.isEmpty(data)) {
+        errors.push("Data doesn't exist.");
+        throw new ErrorHandler(500, errors);
+      }
+
+      // Validate Old Password
+      compareOldPassword = await bcrypt.comparePassword(
+        params.old_password,
+        data.password
+      );
+      if (!compareOldPassword) {
+        errors.push("Old password is incorrect.");
+        throw new ErrorHandler(500, errors);
+      }
+
+      // Validate New Password
+      compareNewPassword = await bcrypt.comparePassword(
+        params.new_password,
+        data.password
+      );
+      if (compareNewPassword) {
+        errors.push("New password cannot be the same as your old password.");
+        throw new ErrorHandler(500, errors);
+      }
+
+      // Pre-setting variables
+      initialValues = _.pick(params, ["password", "updated_at"]);
+      initialValues.password = await bcrypt.hashPassword(
+        initialValues.password
+      );
+      await data.update(initialValues);
+
+      handleSuccess(res, {
+        statusCode: 200,
+        message: "Successfully changed password.",
+        result: [],
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  /**
+   * Find all
+   * @route GET /users
+   */
+  findAll: async (req, res, next) => {
+    let errors = [],data, criteria;
+
+    try {
+      // Validate Data
+      criteria = {
+        attributes: ['email', 'username', 'role_id', 'created_at', 'updated_at', 'permission_type', 'is_logged', 'is_active'],
+        where: { is_deleted: NO },
+        include: [{ model: Model.Roles, as: "roles" }],
+      };
+      data = await Model.Users.findAll(criteria);
+      if (_.isEmpty(data[0])) {
+        errors.push("No data found.");
+        throw new ErrorHandler(500, errors);
+      }
+      
+      handleSuccess(res, {
+        statusCode: 200,
+        message: "Successfully find all data.",
+        result: data,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  /**
+   * Find by id
+   * @route GET /users/:id
+   */
+  findById: async (req, res, next) => {
+    let errors = [],data, criteria;
+
+    try {
+      // Validate Data
+      criteria = { include: [{ model: Model.Roles, as: "roles" }] };
+      data = await Model.Users.findByPk(req.params.id, criteria);
+      if (_.isEmpty(data)) {
+        errors.push("No data found.");
+        throw new ErrorHandler(500, errors);
+      }
+
+      handleSuccess(res, {
+        statusCode: 200,
+        message: "Successfully find data.",
+        result: _.omit(data.get({ plain: true }), ["password", "is_deleted"]),
+      });
+    } catch (err) {
+      next(err);
     }
   },
 };
--- api-backend/src/routes/users.js
+++ api-backend/src/routes/users.js
@@ -10,7 +10,7 @@ router.route('/').get(authController.authorization, usersController.findAll);
 router.route('/create').post(authController.authorization, usersController.create);
 router.route('/update/:id').put(authController.authorization, usersController.update);
 router.route('/delete/:id').put(authController.authorization, usersController.delete);
-router.route('/search/:value').get(authController.authorization, usersController.search);
+router.route('/changePassword/:id').put(authController.authorization, usersController.changePassword);
 router.route('/:id').get(authController.authorization, usersController.findById);
 
 module.exports = router;
